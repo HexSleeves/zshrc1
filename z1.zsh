c5643fcfb1454bdc33bb0352e656c3257df3fdf5
mattmc3 <mattmc3@gmail.com>
2026-08-02 18:28:34 -0400

    feat(History): keep repeated commands and add histsync
    Revamp the dupes settings. We have a big history file - no need to obsess so much over not saving dupes. We still expire them first if the file gets too big.
    
    Add histsync so that cross session history can be synced while the session is still active

--- z1.zsh
+++ z1.zsh
@@ -172,10 +172,9 @@ setopt bang_hist               # Treat the '!' character specially during expans
 setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
 setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
 setopt hist_find_no_dups       # Do not display a previously found event.
-setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
+setopt hist_ignore_dups        # Don't write to history if the prior command is a duplicate.
 setopt hist_ignore_space       # Do not record an event starting with a space.
 setopt hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
-setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
 setopt hist_verify             # Do not execute immediately upon history expansion.
 setopt inc_append_history      # Write to the history file immediately, not when the shell exits.
 setopt NO_hist_beep            # Don't beep when accessing non-existent history.
@@ -197,8 +196,9 @@ zstyle -s ':z1:history' savehist _z1_val && SAVEHIST=$_z1_val
 zstyle -s ':z1:history' histsize _z1_val && HISTSIZE=$_z1_val
 unset _z1_val
 
-# Use a better history command.
+# Helpful history commands.
 alias history='fc -li'
+alias histsync='fc -RI'
 
 #
 # Color
